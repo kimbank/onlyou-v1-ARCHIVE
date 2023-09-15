8d6afc840d8780e0243f553e8fd9023c636f18c2
송재연 <inno339@gmail.com>
2023-09-15 13:28:20 +0900

    sjy-myInfoEdit

--- app/(_게시판_)/board/page.jsx
+++ app/(_게시판_)/board/page.jsx
@@ -1,8 +1,15 @@
+'use client'
 
+import React, { useState, useEffect } from 'react';
+
+import Container from "@mui/material/Container";
 
 export default function Board() {
 
   return (
-    <h1>게시판 페이지입니다.</h1>
-  )
+    <Container>
+      <div className="heading2" style= {{textAlign: 'center'}}>아직 준비중인 기능입니다.</div><br/>
+      <div className='heading6' style={{color: 'grey', textAlign: 'center'}}>조금만 기다려주세요..!</div>
+    </Container>
+  );
 }

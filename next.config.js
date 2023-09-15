93eb6d79c21ffbd40060c24e79eec1a01e9da093
kimbank <kimeunhang@naver.com>
2023-09-16 02:02:28 +0900

    fix: settings related with deploy

--- next.config.js
+++ next.config.js
@@ -4,24 +4,24 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+        destination: "http://127.0.0.1:8000/api/:path*",
+          // process.env.NODE_ENV === "development"
+          //   ? "http://127.0.0.1:8000/api/:path*"
+          //   : "/api/",
       },
       {
         source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+        destination: "http://127.0.0.1:8000/api/:path*",
+          // process.env.NODE_ENV === "development"
+          //   ? "http://127.0.0.1:8000/docs"
+          //   : "/api/docs",
       },
       {
         source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+        destination: "http://127.0.0.1:8000/api/:path*",
+          // process.env.NODE_ENV === "development"
+          //   ? "http://127.0.0.1:8000/openapi.json"
+          //   : "/api/openapi.json",
       },
     ];
   },

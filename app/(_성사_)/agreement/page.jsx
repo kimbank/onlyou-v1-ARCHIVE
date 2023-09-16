1dddd28355b2d3f31eaa6084e36add2fbdec58e4
송재연 <inno339@gmail.com>
2023-09-16 16:46:43 +0900

    sjy: 성사페이지
    성사페이지 첫번쨰 만드는 중입니다

--- app/(_성사_)/agreement/page.jsx
+++ app/(_성사_)/agreement/page.jsx
@@ -1,8 +1,50 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import Container from "@mui/material/Container";
+import { Certification } from '@/components/Notification';
+import { MainHalfButton, MainMiniButton, SubHalfButton, SubMiniButton, SubMiniFullButton } from '@/components/Button';
+import { DefaultCheckbox } from '@/components/Checkbox';
+
+import Image from 'next/image';
+import Bag from "@/public/bag.svg";
+import House from "@/public/house.svg";
+import People from "@/public/people.svg";
+import Typography from '@mui/material/Typography';
+
+import { DormantToggle } from '@/components/Toggle';
+import Modal from '@/components/Modal';
+import { MainButton } from '@/components/Button';
 
 
-export default function Agreement() {
-
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#FFA266',
+        main: '#FF7700',
+        dark: '#C45A00',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#FFFFFF',
+        main: '#F7F4F2',
+        dark: '#B2B0AE',
+        contrastText: '#3C3B3A',
+      },
+    },
+  });
+  
+  export default function RootLayout({ children }) {
     return (
-        <h1>성사페이지 입니다.</h1>
+      <ThemeProvider theme={theme}>
+        <Header />
+            <Container>
+                {/*성사된 상대의 유무에 따라 나타나야 하는 */}
+                <div className='heading2'>아직 성사된 상대가 없어요.</div>
+            </Container>
+        <NavBar />
+      </ThemeProvider>
     )
-}
+  }
+  

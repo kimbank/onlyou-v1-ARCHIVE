3c911022ecfe480ead63e7314b6b887e50ed9e84
Urchinode <vrdhan212@gmail.com>
2023-09-18 22:30:02 +0900

    update: 계산 전반적인 흐름 구현
    특정 필드에 대해 별도 조건문만 처리해주면 된다.
    이후, 하나씩 DB에 저장하기

--- api/utils/score.py
+++ api/utils/score.py
@@ -1,44 +1,93 @@
 def get_score(data, target_data):
+    PENALTY = -60
+    PROMOTION_HATES = ['job_type', 'education']
+    EXTRA_SINGLES = ['athletic_life', 'pet_animal', 'consumption_values', 'preffered_dating',
+                     'preferred_contact_method', 'contact_style', 'skinship', 'sns',
+                     'conflict_resolution_method']  # Extra에서 단일 선택 정보
+    EXTRA_HATES = ['smoking_history', 'religion']  # Extra에서 꺼리는 선지 정보
     target_standard = {}
     score = 0
-    for key, value in data.__dict__.items():
+
+    # 사용자의 이상형 기준 추출
+    for key, value in data.items():
         base_key = key[:-2]
-        if key.endswith('_w') and value is not None:
-            for related_data in data.__dict__.keys():  # 해당 가중치에 대한 모든 정보를 저장 (_s, _e 등등)
+        if key.endswith('_w') and value is not None:  # 가중치가 존재하면
+            for related_data in data.keys():  # 관련 정보 모두 저장(_s, _e 등등)
                 if related_data.startswith(base_key):
                     target_standard[related_data] = data[related_data]
 
     important_standard = {key: value for key, value in target_standard.items() if key.endswith('_w') and value == 5}
 
-    # 여기서부터 다시 시작 9-17-2023
-    # 이후 None이 아닌 이상형 정보(target_standard)를 기준으로 2의 이성 정보와 비교하며 점수 계산
-    for key, value in target_data.items():
-        if key not in target_standard.keys():
+    # 무조건 반영 부합 안 할시 -60점 처리
+    # TODO: 가입 정보: 성별은 이미 필터링 되어 있기 때문에 생략, 미구현(residence 해야함)
+    for key, value in target_data['u'].items():
+        if value is None:
             continue
-
-        # 생년월일과 키는 범위 정보이기 때문에 별도 처리
-        if key == 'date_birth' or key == 'height':
+        if key == 'date_birth':
             if target_standard[key + '_s'] <= value <= target_standard[key + '_e']:
                 score += target_standard[key + '_w']
             elif key in important_standard:
-                return 0
-            else:
-                continue
+                score += PENALTY
+            continue
 
-        # job_name의 경우 이상형 정보가 id로 저장되어 있기 때문에 id를 통해 job_name을 가져온 후 비교
-        elif key == 'job_name_id':
-            if value == target_standard[key]:
-                score += target_standard['job_name_w']
+    # 심사 정보
+    for key, value in target_data['ud'].items():
+        if value is None:
+            continue
+        # 꺼리는 정보
+        # TODO: job_type은 문자열 -> 단일 선택 정보 취급해야할듯
+        if key in PROMOTION_HATES:
+            if value in target_standard[key].split(','):
+                if key in important_standard:
+                    score += PENALTY
+                continue
+            else:
+                score += target_standard[key + '_w']
+
+        elif key == 'height':
+            if target_standard[key + '_s'] <= value <= target_standard[key + '_e']:
+                score += target_standard[key + '_w']
             elif key in important_standard:
-                return 0
-            else:
+                score += PENALTY
+            continue
+
+        elif key == 'divorce':
+            if target_standard[key] == value:
+                score += target_standard[key + '_w']
+            elif key in important_standard:
+                score += PENALTY
+            continue
+
+    # 부가 정보
+    # TODO: interests, religion, fashion_style은 문자열 -> 단일 선택 정보 취급해야할듯
+    # TODO: 근데 religion은 꺼리는 선지임 -> 이건 별도로 처리
+    for key, value in target_data['ue'].items():
+        if value is None:
+            continue
+        # 꺼리는 정보
+        if key in EXTRA_HATES:
+            if value in target_standard[key].split(','):
+                if key in important_standard:
+                    score += PENALTY
                 continue
+            else:
+                score += target_standard[key + '_w']
 
-        elif value == target_standard[key]:
-            score += target_standard[key + '_w']
+        # 단일 선택 정보
+        elif key in EXTRA_SINGLES:
+            if target_standard[key] == value:
+                score += target_standard[key + '_w']
+            elif key in important_standard:
+                score += PENALTY
+            continue
 
-        elif key in important_standard:
-            return 0
+        # 다중 선택 정보
+        else:
+            if value in target_standard[key].split(','):
+                score += target_standard[key + '_w']
+            elif key in important_standard:
+                score += PENALTY
+            continue
 
     return score
 
@@ -46,11 +95,10 @@ def get_score(data, target_data):
 def get_scores(data, targets):
     score_dict = {}
     # 이상형 정보 분류
-    for u, ud, ue, ut in targets:
+    for u, ud, ue in targets:
         target = {}
         target['u'] = u
         target['ud'] = ud
         target['ue'] = ue
-        target['ut'] = ut
         score_dict[u.id] = get_score(data, target)
     return score_dict

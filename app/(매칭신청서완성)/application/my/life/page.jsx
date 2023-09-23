def74159ca3207429b00bdb75961dfe72d396e6a
LeeEuiHyoung <kkh60245@gmail.com>
2023-09-23 16:57:29 +0900

    leh/2. 음주 생활 추가

--- app/(매칭신청서완성)/application/my/life/page.jsx
+++ app/(매칭신청서완성)/application/my/life/page.jsx
@@ -9,35 +9,10 @@ import { DropdownInput } from "@/components/Input";
 import { MainButton, SubMiniButton, SubButton } from "@/components/Button";
 
 import SmokingHistory from "@/components/survey/my/smoking_history";
+import Drinking from "@/components/survey/my/drinking";
 
 const Value = () => {
   const [data, setData] = React.useState(LifeData);
-
-  // const editButtonNameGrop = [
-  //   "여행",
-  //   "운동/스포츠",
-  //   "책",
-  //   "직무",
-  //   "전시회",
-  //   "외국/언어",
-  //   "영화/넷플릭스",
-  //   "공예/만들기",
-  //   "음악/악기",
-  //   "콘서트/공연/뮤지컬",
-  //   "재태크",
-  //   "댄스/무용",
-  //   "전시회",
-  //   "봉사",
-  //   "사교/인맥",
-  //   "차/오토바이",
-  //   "반려동물",
-  //   "게임/오락",
-  //   "요리",
-  //   "사진/영상",
-  //   "맛집/카페",
-  //   "에니메이션/만화",
-  //   "쇼핑/패션",
-  // ];
 
   return (
     <Container
@@ -50,14 +25,10 @@ const Value = () => {
     >
       <button onClick={() => console.log(data)}>정보 보기</button>
 
-
-
       <Typography className="heading2"> 생활 정보 입력하기 </Typography>
 
-
-
       <SmokingHistory data={data} setData={setData} />
-      {/* <Drinking data={data} setData={setData} /> */}
+      <Drinking data={data} setData={setData} />
       {/* <OwnedCar data={data} setData={setData} /> */}
       {/* <Interests data={data} setData={setData} /> */}
       {/* <NumberRelationships data={data} setData={setData} /> */}
@@ -65,38 +36,6 @@ const Value = () => {
       {/* <PetAnimal data={data} setData={setData} /> */}
       {/* <Religion data={data} setData={setData} /> */}
 
-
-
-
-
-      {/* <Container
-        disableGutters
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "32px",
-        }}
-      >
-        <DropdownInput buttonName={"흡연 경력"} />
-        <DropdownInput buttonName={"음주 생활"} />
-        <DropdownInput buttonName={"자차 보유"} />
-        <Typography className="input-title">관심사(모두 선택)</Typography>
-        <Container
-          sx={{
-            display: "flex",
-            flexFlow: "row wrap",
-            gap: "8px",
-          }}
-        >
-          {editButtonNameGrop.map((buttonName) => (
-            <SubMiniButton buttonName={buttonName} />
-          ))}
-        </Container>
-        <DropdownInput buttonName={"연애 횟수"} />
-        <DropdownInput buttonName={"운동 생활"} />
-        <DropdownInput buttonName={"반려동물"} />
-        <DropdownInput buttonName={"종교"} />
-      </Container> */}
       <Container
         disableGutters
         sx={{

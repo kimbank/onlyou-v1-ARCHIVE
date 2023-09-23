cbb6c3c677af223536a999eac25368e8470ee495
LeeEuiHyoung <kkh60245@gmail.com>
2023-09-24 00:30:00 +0900

    leh/19. 관심사 버튼 항목 추가

--- app/(매칭신청서완성)/application/my/life/page.jsx
+++ app/(매칭신청서완성)/application/my/life/page.jsx
@@ -5,10 +5,11 @@ import Link from "next/link";
 import Container from "@mui/material/Container";
 import { Typography } from "@mui/material";
 
+import Interests from "@/app/(매칭신청서완성)/application/my/life/interests";
+
 import { MainButton, SubMiniButton, SubButton } from "@/components/Button";
 import { DangerNotification } from '@/components/Notification';
 
-// import Interests from "@/components/survey/my/life/interests";
 import { DropDownInput } from "@/components/survey/my/drop_down_input";
 
 function canProceedToNextPage(data) {
@@ -46,10 +47,10 @@ const Life = () => {
         flexDirection: "column",
         gap: "32px"
       }}>
-        <DropDownInput data={data} setData={setData} data_name={"smoking_history"} title={"흡연 경력"} options={["비흡연", "금연", "흡연"]} />
+        <DropDownInput data={data} setData={setData} data_name={"smoking_history"} title={"흡`연 경력"} options={["비흡연", "금연", "흡연"]} />
         <DropDownInput data={data} setData={setData} data_name={"drinking_life"} title={"음주 생활"} options={["전혀 마시지 않음", "거의 마시지 않음", "이따금 마심(한 달 1회 이상)", "종종 마심(주 1회 이상)", "자주 마심(주 2회 이상)"]} />
         <DropDownInput data={data} setData={setData} data_name={"owned_car"} title={"자차 유무"} options={["미소유", "소유"]} />
-        {/* issue: Interests 미정 */}
+        <Interests data={data} setData={setData} />
         <DropDownInput data={data} setData={setData} data_name={"number_relationships"} title={"연애 횟수"} options={["없음", "1~2회", "3~4회", "5~6회", "7회 이상"]} />
         <DropDownInput data={data} setData={setData} data_name={"athletic_life"} title={"운동 생활"} options={["중요성엔 공감하지만, 규칙적으로 하고 있진 않다", "운동을 규칙적으로 꾸준히 한다"]} />
         <DropDownInput data={data} setData={setData} data_name={"pet_animal"} title={"반려동물"} options={["키우기 어렵습니다", "키우지 않으나 반려동물에 거부감은 없습니다", "한 마리 키웁니다", "두 마리 이상 키웁니다."]} />
@@ -83,7 +84,7 @@ const LifeData = {
   smoking_history: null,
   drinking_life: null,
   owned_car: null,
-  interests: -1, // issue: Interests 미정
+  interests: null, // issue: Interests 미정
   number_relationships: null,
   athletic_life: null,
   pet_animal: null,

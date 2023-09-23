2893240beefa1f1f699559615312bc90c40dcd89
kimbank <kimeunhang@naver.com>
2023-09-23 23:12:26 +0900

    feat: 매칭신청서 완성 생활정보 업데이트

--- components/survey/target/drinking_life.jsx
+++ components/survey/target/drinking_life.jsx
@@ -7,13 +7,13 @@ import { Typography, Container, Button, Divider } from "@mui/material";
 
 import WeightPannel from "./weight_pannel";
 
-export default function Education({ data, setData, sub, setSub }) {
+export default function DrinkingLife({ data, setData, sub, setSub }) {
   const [value, setValue] = React.useState("");
   const [weight, setWeight] = useState(null);
 
   useEffect(() => {
-    if (value == "") { setData({ ...data, education: null }); }
-    else { setData({ ...data, education: value }); }
+    if (value == "") { setData({ ...data, drinking_life: null }); }
+    else { setData({ ...data, drinking_life: value }); }
   }, [value]);
 
   const handleChange = (newValue) => {
@@ -32,8 +32,8 @@ export default function Education({ data, setData, sub, setSub }) {
         setValue(d.sort().join(','))
       }
     }
-
     setSub(!sub);
+    // setData({ ...data, education: value });
   };
 
   const handleWeight = (newWeight) => {
@@ -42,7 +42,7 @@ export default function Education({ data, setData, sub, setSub }) {
     }
     else {
       setWeight(newWeight);
-      setData({ ...data, education_w: newWeight });
+      setData({ ...data, drinking_life_w: newWeight });
     }
     setSub(!sub);
   };
@@ -57,10 +57,10 @@ export default function Education({ data, setData, sub, setSub }) {
         gap: "8px",
       }}
     >
-    <Typography className="heading2">학력</Typography>
+    <Typography className="heading2">음주 횟수</Typography>
     <Divider />
     <Typography className="basic-gray">
-      원하는 상대방의 학력을 모두 선택해주세요.
+      원하는 상대방의 음주 횟수를 모두 선택해주세요.
     </Typography>
 
 
@@ -73,11 +73,11 @@ export default function Education({ data, setData, sub, setSub }) {
         gap: "8px",
       }}
     >
-      <Button variant="contained" sx={sx} onClick={() => handleChange('0')} color={value.split(',').includes('0') ? "primary" : "secondary"}>대학 미진학</Button>
-      <Button variant="contained" sx={sx} onClick={() => handleChange('1')} color={value.split(',').includes('1') ? "primary" : "secondary"}>전문대</Button>
-      <Button variant="contained" sx={sx} onClick={() => handleChange('2')} color={value.split(',').includes('2') ? "primary" : "secondary"}>일반 4년제 대학</Button>
-      <Button variant="contained" sx={sx} onClick={() => handleChange('3')} color={value.split(',').includes('3') ? "primary" : "secondary"}>명문대</Button>
-      <Button variant="contained" sx={sx} onClick={() => handleChange('4')} color={value.split(',').includes('4') ? "primary" : "secondary"}>일류대</Button>
+      <Button variant="contained" sx={sx} onClick={() => handleChange('0')} color={value.split(',').includes('0') ? "primary" : "secondary"}>마시지 않음</Button>
+      <Button variant="contained" sx={sx} onClick={() => handleChange('1')} color={value.split(',').includes('1') ? "primary" : "secondary"}>거의 마시지 않음</Button>
+      <Button variant="contained" sx={sx} onClick={() => handleChange('2')} color={value.split(',').includes('2') ? "primary" : "secondary"}>이따금 마심(한달 1회이상)</Button>
+      <Button variant="contained" sx={sx} onClick={() => handleChange('3')} color={value.split(',').includes('3') ? "primary" : "secondary"}>종종 마심(주 1회이상)</Button>
+      <Button variant="contained" sx={sx} onClick={() => handleChange('4')} color={value.split(',').includes('4') ? "primary" : "secondary"}>자주 마심(주 2회이상)</Button>
     </Container>
 
 
---
+++ components/survey/target/pet_animal.jsx
@@ -0,0 +1,93 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { Typography, Container, Button, Divider } from "@mui/material";
+
+import WeightPannel from "./weight_pannel";
+
+export default function PetAnimal({ data, setData, sub, setSub }) {
+  const [value, setValue] = React.useState("");
+  const [weight, setWeight] = useState(null);
+
+  useEffect(() => {
+    if (value == "") { setData({ ...data, pet_animal: null }); }
+    else { setData({ ...data, pet_animal: value }); }
+  }, [value]);
+
+  const handleChange = (newValue) => {
+    if (newValue == value) {
+      setValue("")
+    }
+    else if (value == "") {
+      setValue(newValue)
+    } else {
+      let d = value.split(',').sort(function(a,b) {return Number(a)-Number(b)})
+      if (d.includes(newValue)) {
+        d.splice(d.indexOf(newValue), 1)
+        setValue(d.join(','))
+      } else {
+        d.push(newValue)
+        setValue(d.sort(function(a,b) {return Number(a)-Number(b)}).join(','))
+      }
+    }
+    setSub(!sub);
+  };
+
+  const handleWeight = (newWeight) => {
+    if (newWeight == weight) {
+      handleWeight(null);
+    }
+    else {
+      setWeight(newWeight);
+      setData({ ...data, pet_animal_w: newWeight });
+    }
+    setSub(!sub);
+  };
+
+  return (
+    <Container
+      disableGutters
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        gap: "8px",
+      }}
+    >
+      <Typography className="heading2">관심사</Typography>
+      <Divider />
+      <Typography className="basic-gray">
+        원하는 상대방의 관심사를 최대 3개 선택해주세요.
+      </Typography>
+
+
+      <Container
+        disableGutters
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: "8px",
+          overflowX: "scroll",
+        }}
+      >
+        <Button variant="contained" sx={sx} onClick={() => handleChange('0')} color={value.split(',').includes('0') ? "primary" : "secondary"}>키우기 어렵습니다</Button>
+        <Button variant="contained" sx={sx} onClick={() => handleChange('1')} color={value.split(',').includes('1') ? "primary" : "secondary"}>키우지 않으나 반려동물에 거부감은 없습니다.</Button>
+        <Button variant="contained" sx={sx} onClick={() => handleChange('2')} color={value.split(',').includes('2') ? "primary" : "secondary"}>한마리 키웁니다</Button>
+        <Button variant="contained" sx={sx} onClick={() => handleChange('3')} color={value.split(',').includes('3') ? "primary" : "secondary"}>두마리 이상 키웁니다</Button>
+      </Container>
+
+
+      <WeightPannel weight={weight} handleWeight={handleWeight} />
+    </Container>
+  );
+}
+
+const sx = {
+  borderRadius: "8px",
+  minHeight: "35px",
+  minWidth: "40px",
+  boxShadow: "none",
+}
